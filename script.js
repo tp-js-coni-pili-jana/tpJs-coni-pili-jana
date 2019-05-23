c9da41ed92f2070ca1baa8ddbb290ad3c1d74f28
Coni <coniarganaraz@hotmail.com>
2019-05-22 22:28:57 -0300

    iconos agregados

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// esto lo hice tomando como ejemplo el tweet
-
 var task
 var newTask
 var pendingList
@@ -16,13 +14,11 @@ var printTask = function(){
   doneList = document.getElementById("doneList")
   doneList.innerHTML = ""
   allTask.map(function(assignment, index){
-    // crear variable aparte luego
       var taskItem = document.createElement("li")
       taskItem.classList.add("assignment")
       taskItem.innerText = assignment.text
-      taskItem.appendChild(createBtn('toggle', index, 'check', toggleItem))
-      taskItem.appendChild(createBtn('delete', index, 'bin' ,deleteItem))
-      // 
+      taskItem.appendChild(createBtn('<i class="far fa-check-square"></i>', index, "check", toggleItem))
+      taskItem.appendChild(createBtn('<i class="far fa-trash-alt"></i>', index, 'bin' ,deleteItem))
       if(assignment.pending){
         pendingList.appendChild(taskItem)
         } else {
@@ -46,27 +42,20 @@ var sendTask = function(){
   }
 }
 
-// asi se creaba el elemento aparte, RECORDAR:
-
-//var createItem = function(text, index){
-//  var li = document.createElement('li')
- // li.innerText = text
- // li.appendChild(createBtn('toggle', index, toggleItem))
- // li.appendChild(createBtn('delete', index, deleteItem))
- // return li
-//} 
 
 // designacion de responsabilidades
 
 var createBtn = function(text, itemId,nameClass, btnFuction){
   var btn = document.createElement('a')
-  btn.innerText = text
+  btn.innerHTML = text
   btn.id = itemId
   btn.classList.add(nameClass)
   btn.href = "#"
   btn.onclick = function(){ btnFuction(this) }
   return btn
 }
+
+
 
 var toggleItem = function(btn){
   allTask[btn.id].pending = !allTask[btn.id].pending
